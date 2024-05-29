d3e9e55db5dabacb4e99ad32674c40945c49008d
sergio15dev <sergio15.dev@gmail.com>
2024-05-29 13:19:46 -0500

    frontend: update views components in tabs

--- expertus/app/(tabs)/_layout.tsx
+++ expertus/app/(tabs)/_layout.tsx
@@ -23,6 +23,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen 
+        name="chatbox"
+        options={{
+          title: 'Chatbox',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'chatbox' : 'chatbox-outline'} color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="explore"
         options={{
